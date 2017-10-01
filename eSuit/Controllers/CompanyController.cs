c244e507772ae2fd2c884646f39e5bb458e051ac
Waqas Akhtar <waqas_viki@yahoo.com>
2017-10-01 12:01:01 +0500

    Initial Commit.....

---
+++ eSuit/Controllers/CompanyController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eSuitRepository.DataModels;
+using eSuitRepository.DataProviders;
+
+namespace eSuit.Controllers
+{
+    public class CompanyController : Controller
+    {
+        //
+        // GET: /Company/
+        public ActionResult Index()
+        {
+            Company dpCompany = new Company();
+            return View(dpCompany.List());
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+    }
+}

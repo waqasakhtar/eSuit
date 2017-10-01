4a991fda54e44760a21e65cd4b8014c65758183d
Waqas Akhtar <waqas_viki@yahoo.com>
2017-10-01 18:33:13 +0500

    Changes Continued.....

--- eSuit/dist/js/export-table-data.js
+++ eSuit/dist/js/export-table-data.js
@@ -1,12 +1,24 @@
 /*Export Table Init*/
 
-"use strict"; 
+"use strict";
 
-$(document).ready(function() {
-	$('#example').DataTable( {
-		dom: 'Bfrtip',
-		buttons: [
-			'copy', 'csv', 'excel', 'pdf', 'print'
-		]
-	} );
-} );
+//$(document).ready(function () {
+//    $('#example').DataTable({
+//        dom: 'Bfrtip',
+//        buttons: [
+//			'copy', 'csv', 'excel', 'pdf', 'print'
+//        ]
+//    });
+//});
+
+
+$(document).ready(function () {
+    $('#example').DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+			'excel', 'print'
+        ]
+    });
+
+});
+
